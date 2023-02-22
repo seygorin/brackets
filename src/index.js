880d123ea5ebd53590685f9e712414194162542d
Sergey Gorin <seygorin@gmail.com>
2023-02-22 15:04:07 +0500

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const stack = [];
+  const openingBrackets = bracketsConfig.map(pair => pair[0]);
+  const closingBrackets = bracketsConfig.map(pair => pair[1]);
+
+  for (const bracket of str) {
+    if (openingBrackets.includes(bracket)) {
+      if (closingBrackets.includes(bracket) && stack[stack.length - 1] === bracket) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    } else if (closingBrackets.includes(bracket)) {
+      const openingBracket = openingBrackets[closingBrackets.indexOf(bracket)];
+      if (stack[stack.length - 1] === openingBracket) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
